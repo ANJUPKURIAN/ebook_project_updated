297f9cb31af268c85c3ba80e2b56e5d9674c3160
anju <anjupkurian199718@gmail.com>
2024-06-22 16:42:35 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/ebook_project_updated/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ebook_project_updated
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants/colorconstants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  // for pagedesign & letters
+  static const Color primaryblue = Color(0xff3797EF);
+  static const Color primaryblack = Color(0xff000000);
+  static const Color primarywhite = Color(0xffffffff);
+  static const Color primarygrey = Color(0xffFAFAFA);
+  
+
+// for icons
+    static const Color normalBlue=Colors.blue;
+    static const Color normalGrey = Colors.grey;
+    static const Color normalYellow=Colors.yellow;
+   static const Color normalRed = Colors.red;
+  
+}
